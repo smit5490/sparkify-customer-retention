e145ac30fac70cf9a421699955cb8e3c2e5c1661
smit5490 <smit5490@morris.umn.edu>
2022-01-24 19:57:46 -0600

    notebook and package updates

--- src/sparkifychurn/exploreData.py
+++ src/sparkifychurn/exploreData.py
@@ -18,10 +18,13 @@ def summarize_missing_vals(df):
     missing_pct_list = []
     empty_cnt_list = []
     empty_pct_list = []
+    dtype_list = []
     total_cnt = df.count()
 
     for feature in df.columns:
-        if df.select(feature).dtypes[0][1] == "timestamp":
+        feat_type = df.select(feature).dtypes[0][1]
+        dtype_list.append(feat_type)
+        if (feat_type == "timestamp") | (feat_type == "date"):
             miss_cnt = df.filter(F.isnull(feature)).count()
             missing_cnt_list.append(miss_cnt)
             missing_pct_list.append(round((miss_cnt / total_cnt) * 100, 2))
@@ -31,16 +34,17 @@ def summarize_missing_vals(df):
             missing_cnt_list.append(miss_cnt)
             missing_pct_list.append(round((miss_cnt / total_cnt) * 100, 2))
 
-        if df.select(feature).dtypes[0][1] == "string":
+        if feat_type == "string":
             empty_cnt = df.filter(F.trim(F.col(feature)) == "").count()
             empty_cnt_list.append(empty_cnt)
             empty_pct_list.append(round((empty_cnt / total_cnt) * 100, 2))
         else:
-            empty_cnt_list.append(0)
-            empty_pct_list.append(0)
+            empty_cnt_list.append(None)
+            empty_pct_list.append(None)
         print("****Feature {} complete.****".format(feature))
 
     missing_summary = pd.DataFrame({"feature": df.columns,
+                                    "data_type": dtype_list,
                                     "missing_count": missing_cnt_list,
                                     "missing_percentage": missing_pct_list,
                                     "empty_string_count": empty_cnt_list,
